63533dcc44cd4f8f3f0e83d0f10a9b5d3361195f
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-06-16 19:08:27 +0300

    decryption process
    - inverse of the encryption

--- decryptor.py
+++ decryptor.py
@@ -16,8 +16,17 @@ def encrypt_message(message, key_matrix):
     return encrypted_vector
 
 
+def decrypt_message(encrypted_vector, key_matrix):
+    eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
+    diagonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
+    decrypted_vector = np.dot(np.linalg.inv(diagonalized_key_matrix), encrypted_vector)
+    return ''.join([chr(int(round(num))) for num in decrypted_vector.real])
+
+
 message = "Meaningful Message."
 print("Original message: ", message)
 key_matrix = np.random.randint(0, 256, (len(message), len(message)))
 encrypted_message = encrypt_message(message, key_matrix)
 print("Encrypted message: ", encrypted_message)
+decrypted_message = decrypt_message(encrypted_message, key_matrix)
+print("Decrypted message: ", decrypted_message)
